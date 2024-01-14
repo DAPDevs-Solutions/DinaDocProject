4625e5223af6883c79ffea047cea9b3b9ef6a507
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2024-01-14 12:23:54 +0700

    Changes: - the code from ContactPage has been replaced

--- menu_pages/views.py
+++ menu_pages/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
-from .models import Menu, Category
+from django.urls import reverse_lazy
+from .models import Menu, ContactUs, Category
 from .forms import FeedbackForm
 
 menu = Menu.objects.all()
@@ -17,27 +18,18 @@ class MainPage(TemplateView):
         return context
 
 
-class ContactPage(TemplateView):
+class ContactPage(CreateView):
+    model = ContactUs
+    form_class = FeedbackForm
     template_name = 'contact_page/contact_page.html'
-    context_object_name = 'menu',
+    context_object_name = 'menu'
+    success_url = reverse_lazy('contact_page')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu'] = menu
+        context['feedback_form'] = self.form_class()
         return context
-
-    @staticmethod
-    def feedback(request):
-        if request.method == 'POST':
-            feedback_form = FeedbackForm(request.POST)
-            if feedback_form.is_valid():
-                feedback_form.save()
-                return redirect('contact_page')
-        else:
-            feedback_form = FeedbackForm()
-
-        context = {'feedback_form': feedback_form}
-        return render(request, 'contact_page.html', context)
 
 
 class ServicesPage(TemplateView):
