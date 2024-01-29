80f30050a4ecabe0cad2cbfd487b1d0ae51db90e
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2024-01-29 13:05:44 +0700

    Update media files

--- menu_pages/static/js/menu_list.js
+++ menu_pages/static/js/menu_list.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const items = document.querySelectorAll('.menu_list--list_items .item a');
+    const items = document.querySelectorAll('.menu--tabs_items .item a');
     const liHref = window.location.pathname.replace(/\/$/, '');
     const cleanLiHref = liHref.replace(/^\/[a-z]{2}(\/|$)/, '/');
 
