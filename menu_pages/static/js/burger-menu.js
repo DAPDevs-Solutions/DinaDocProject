0fe3da249b8b4a63008776571c0e2d9a0bd44e89
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2024-01-31 13:09:12 +0700

    Creating burger menu

--- menu_pages/static/js/burger-menu.js
+++ menu_pages/static/js/burger-menu.js
@@ -1,5 +1,11 @@
-document.querySelector('.burger-menu').addEventListener('click', function () {
-    this.classList.toggle('active');
-    document.querySelector('.navigation').classList.toggle('open');
+// document.querySelector('.burger-menu').addEventListener('click', function () {
+//     this.classList.toggle('active');
+//     document.querySelector('.navigation').classList.toggle('open');
+//
+// })
 
-})
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("burger").addEventListener("click", function () {
+        document.querySelector(".navigation").classList.toggle("open");
+    });
+});
