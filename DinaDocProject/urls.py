6e7f8176eadfb85915e4a5579cbe7ffb9071b302
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2024-02-18 23:33:54 +0700

    Media is completed

--- DinaDocProject/urls.py
+++ DinaDocProject/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.utils.translation import gettext_lazy as _
 
+from django.views.static import serve as mediaserve
+
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
