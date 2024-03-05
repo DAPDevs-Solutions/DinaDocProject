8065d80014fe07f0afb963620047cc7c3ffdc2f3
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2024-03-05 20:33:21 +0700

    Updated: - changed js code for burger menu animation. - added open animation for navigation block

--- menu_pages/static/js/burger-menu.js
+++ menu_pages/static/js/burger-menu.js
@@ -1,8 +1,13 @@
-document.querySelector('.burger-menu').addEventListener('click', function () {
-    this.classList.toggle('active');
-    document.querySelector('.navigation').classList.toggle('open');
+// document.querySelector('.burger-menu').addEventListener('click', function () {
+//     this.classList.toggle('active');
+//     document.querySelector('.navigation').classList.toggle('open');
+//
+// })
 
-})
+document.querySelector('.burger-menu').addEventListener('click', function() {
+  document.querySelector('.navigation').classList.toggle('active');
+  document.querySelector('.burger-menu').classList.toggle('active');
+});
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     document.getElementById("burger").addEventListener("click", function() {
